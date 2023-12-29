4cf1e5c97521878fa04c27482eb14f549121c405
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-29 15:49:42 +0530

    Initial

---
+++ sound.js
@@ -0,0 +1,23 @@
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+function playSound(soundUrl) {
+    const request = new XMLHttpRequest();
+    request.open('GET', soundUrl, true);
+    request.responseType = 'arraybuffer';
+
+    
+    request.onload = function () {
+        audioContext.decodeAudioData(request.response, function (buffer) {
+            const source = audioContext.createBufferSource();
+            source.buffer = buffer;
+            source.connect(audioContext.destination);
+            source.start(0);
+        });
+    };
+
+    request.send();
+}
+
+
+playSound('button-click.mp3');
