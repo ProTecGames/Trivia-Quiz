941bf97617b9e386ca1347c1176ed49efcaf2ecd
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-29 15:54:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+    // Retrieve user data from local storage
     let points = parseInt(localStorage.getItem('points')) || 0;
     const questionsAttempted = parseInt(localStorage.getItem('questionsAttempted')) || 0;
 
-    
+    // Update header with user data
     document.getElementById('points').innerText = `Points: ${points}`;
     document.getElementById('attempted').innerText = `Questions Attempted: ${questionsAttempted}`;
 
-
-  
+    // Example: Populate the level timeline dynamically with buttons
     const levelTimeline = document.getElementById('levelTimeline');
-    const level1Button = createLevelButton(1, 'one/one.html', points > 20);
-    const level2Button = createLevelButton(2, 'two/two.html', points > 50);
-    const level3Button = createLevelButton(3, 'three/three.html', points > 100);
+    const level1Button = createLevelButton(1, '/level/one/one.html', true);
+    const level2Button = createLevelButton(2, '/level/two.html', points > 20);
+    const level3Button = createLevelButton(3, '/level/three.html', points > 50);
 
     levelTimeline.appendChild(level1Button);
     levelTimeline.appendChild(level2Button);
     levelTimeline.appendChild(level3Button);
 
-    
+    // Function to create level buttons
     function createLevelButton(level, link, isEnabled) {
         const button = document.createElement('button');
         button.innerText = `Level ${level}`;
         button.disabled = !isEnabled;
 
-        
+        // Strikeout the text for higher-level buttons until the user has enough points
+        if (!isEnabled) {
+            button.classList.add('strike');
+        }
+
+        // Enable the button only if the user has enough points
         if (isEnabled) {
             button.addEventListener('click', function () {
-                
+                // Play button click sound
                 playButtonClickSound();
-                
-                
+
+                // Redirect to the specified link
                 window.location.href = link;
             });
         }
@@ -38,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return button;
     }
 
-    
+    // Function to play button click sound
     function playButtonClickSound() {
         const buttonClickSound = new Audio('button-click.mp3'); // Replace with your sound file
         buttonClickSound.play();
